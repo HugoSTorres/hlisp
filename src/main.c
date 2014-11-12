74d58a94cd499c438247bb50aed1e796fd6119d9
Hugo Torres <hugo.torres1993@gmail.com>
2014-11-12 10:24:09 -0500

    Finished IO routines

---
+++ src/main.c
@@ -0,0 +1,29 @@
+#include "io.h"
+
+int main(int argc, char* argv[])
+{
+#ifdef DEBUG
+	printf("---- DEBUG MODE ----\n");
+#endif
+
+	// parse command line args
+	int _infile = 0;
+	int c;
+	char *infilename = NULL;
+
+	while((c = getopt(argc, argv, "f:")) > 0){
+		switch(c)
+		{
+			case 'f':
+				_infile = 1;
+				infilename = optarg;
+				break;
+		}
+	}
+
+	// user specified an input file so open, read and close it
+	if (_infile)
+		readf(infilename);
+
+	return 0;
+}
