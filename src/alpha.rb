e203425633a74869804fd72ea258c692e26caa8e
Hugo Torres <hugo.torres1993@gmail.com>
2014-12-03 16:22:11 -0500

    Moved evaluation responsibilities to respective non-terminal

--- src/alpha.rb
+++ src/alpha.rb
@@ -4,4 +4,8 @@ class Alpha
   def initialize(code)
     @value = code
   end
+
+  def self.evaluate(code)
+    /^\w*[a-zA-Z]\w+$/ === code ? Alpha.new(code) : false
+  end
 end
--- src/number.rb
+++ src/number.rb
@@ -2,6 +2,10 @@ class Number
   attr_reader :value
 
   def initialize(code)
-    @value = code.to_i if /^\d+$/ === code
+    @value = code.to_i
+  end
+
+  def self.evaluate(code)
+    /^\d+$/ === code ? Number.new(code) : false
   end
 end
