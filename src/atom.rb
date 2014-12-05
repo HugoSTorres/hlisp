1895d1a5c29153dd53897631791d43f633646a03
Hugo Torres <hugo.torres1993@gmail.com>
2014-12-05 15:12:38 -0500

    Fixed Atom.evaluate to return new instance of Atom

--- src/atom.rb
+++ src/atom.rb
@@ -1,9 +1,15 @@
 class Atom
   attr_reader :value
 
-  def initialize(code)
-    unless (@value = Number.evaluate code)
-      @value = Alpha.evaluate code
+  def initialize(terminal)
+    @value = terminal
+  end
+
+  def self.evaluate(code)
+    unless (terminal = Number.evaluate code)
+      terminal = Alpha.evaluate code
     end
+
+    Atom.new terminal
   end
 end
