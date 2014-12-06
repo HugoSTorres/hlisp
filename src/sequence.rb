b85acfad23a26b6320b239e5557b72283ae96421
Hugo Torres <hugo.torres1993@gmail.com>
2014-12-06 12:15:01 -0500

    Sequence.evaluate creates execution stack, still buggy

--- src/sequence.rb
+++ src/sequence.rb
@@ -5,14 +5,48 @@ class Sequence
     @value = code
   end
 
-  def self.evaluate(code)
-    paren_count = 0
 
-    code.each_char do |c|
-      paren_count = paren_count + 1 if c == '('
-      paren_count = paren_count - 1 if c == ')'
+  def self.evaluate(code)
+    #return false unless check_parenthesis_in code
+
+    # Arrays have stack methods, so we can use one as a stack by just sticking
+    # to the stack methods.
+    exec_stack = []
+    roffset = 0
+    length = code.length
+
+    # Evaluating a sequence involves taking a chunk of code between
+    # parenthesis and pushing it onto the stack. SExpression will take the
+    # stack and pop each element off, evaluating it one at a time.
+    code.each_char.with_index do |c, i|
+      if c == '('
+        code.reverse.each_char.with_index(roffset) do |d, j|
+          if d == ')'
+            roffset = j
+          end
+        end
+
+        # push a new sequence onto the stack, trimmed of parenthesis
+        exec_stack << Sequence.new(code[i, length - roffset])
+      end
     end
 
-    paren_count == 0 ? Sequence.new(code) : false
+    return exec_stack
   end
+
+  private 
+
+    # check sequence's parenthetical validity
+    def check_parenthesis_in(code)
+      paren_count = 0
+
+      return false unless code[0] == '(' && code[code.length - 1] == ')'
+
+      code.each_char do |c|
+        paren_count = paren_count + 1 if c == '('
+        paren_count = paren_count - 1 if c == ')'
+      end
+
+      paren_count == 0 ? true : false
+    end
 end
